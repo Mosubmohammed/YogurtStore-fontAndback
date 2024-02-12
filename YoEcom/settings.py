813501b55643c302ba82d1a9faeeb4e5ddb905af
Mosubmohammed <mosup.050@gmail.com>
2024-02-12 12:45:04 +0300

    .

--- YoEcom/settings.py
+++ YoEcom/settings.py
@@ -81,23 +81,23 @@ WSGI_APPLICATION = 'YoEcom.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/5.0/ref/settings/#databases
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
-}
-
-# DATABASES = {   
-#         'default': {
-#             'ENGINE': 'django.db.backends.postgresql_psycopg2', 
-#             'NAME': 'Yogurtdata',       
-#             'USER': 'admin2',       
-#             'PASSWORD': 'mosup12345',       
-#             'HOST': '127.0.0.1',       
-#             'PORT': '5432',   
-#         }
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': BASE_DIR / 'db.sqlite3',
 #     }
+# }
+
+DATABASES = {   
+        'default': {
+            'ENGINE': 'django.db.backends.postgresql_psycopg2', 
+            'NAME': 'yogurtdata',       
+            'USER': 'admin2',       
+            'PASSWORD': 'mosup12345',       
+            'HOST': '127.0.0.1',       
+            'PORT': '5432',   
+        }
+    }
 
 
 
